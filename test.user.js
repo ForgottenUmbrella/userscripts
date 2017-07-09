7af7b742f320037b17ed046bafbc5e6de48426e3
Leo Pham <ForgottenUmbrella@users.noreply.github.com>
2017-07-09 17:42:57 +1000

    Create test.user.js
    For testing Greasy Fork's syncing capabilities

---
+++ test.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @description Dummy script for testing Greasy Fork's syncing capabilities
+// @name        Test
+// @version     1
+// @namespace https://greasyfork.org/users/83187
+// ==/UserScript==
+
+console.log("Hi");
