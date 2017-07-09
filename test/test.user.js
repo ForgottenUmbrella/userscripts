c7fc2324f194fd7e5b9823932ae4a53a960ee652
Leo Pham <ForgottenUmbrella@users.noreply.github.com>
2017-07-09 17:54:38 +1000

    Update test.user.js
    Test syncing

--- test/test.user.js
+++ test/test.user.js
@@ -3,12 +3,12 @@
 // @description     Dummy script for testing Greasy Fork's syncing capabilities
 // @author          ForgottenUmbrella
 // @namespace       https://greasyfork.org/users/83187
-// @version         1
+// @version         2
 // @match           *
 // ==/UserScript==
 
 (function()
 {
     "use strict";
-    console.log("Hi");
+    console.log("Sync test");
 })();
